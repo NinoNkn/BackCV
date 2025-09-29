667b8bd4e6c4443fbffa196b430ccf9ebb6cd633
Santiago Morales Orozco <moralesorozcosantiago@gmail.com>
2025-09-29 16:51:48 -0500

    Urls trade

--- contacts/urls.py
+++ contacts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ContactCreateView
 
 urlpatterns = [
-    path("contacts/", ContactCreateView.as_view(), name="contacts"),
+    path('contacts/', ContactCreateView.as_view(), name='contact-create'),
 ]
