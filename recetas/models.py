8c912145040710d8a10112ec364aa8747e1a50ba
Santiago Morales Orozco <moralesorozcosantiago@gmail.com>
2025-09-30 14:37:23 -0500

    waza

--- recetas/models.py
+++ recetas/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 class Receta(models.Model):
     titulo = models.CharField(max_length=200)
-    resumen = models.TextField(max_length=300)
+    resumen = models.TextField()
     descripcion = models.TextField()
-    palabras_clave = models.CharField(max_length=200)
-    imagen_url = models.URLField(max_length=500)
+    imagen_url = models.URLField("Imagen (URL)", blank=True, null=True)
+    palabras_clave = models.CharField(max_length=250, help_text="Separar con comas")
 
     def __str__(self):
         return self.titulo
+
--- recetas/views.py
+++ recetas/views.py
@@ -2,6 +2,14 @@ from django.http import JsonResponse
 from .models import Receta
 
 def recetas_api(request):
-    recetas = list(Receta.objects.values("id", "titulo", "resumen", "descripcion", "imagen_url", "palabras_clave"))
+    recetas = list(
+        Receta.objects.values(
+            "id",
+            "titulo",
+            "resumen",
+            "descripcion",
+            "imagen_url",
+            "palabras_clave"
+        )
+    )
     return JsonResponse({"recetas": recetas})
-
