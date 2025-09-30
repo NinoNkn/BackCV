e3c409993a484354865f9aac600aeed8da31a7b8
Santiago Morales Orozco <moralesorozcosantiago@gmail.com>
2025-09-30 12:09:24 -0500

    arreglos URLS

--- faqs/urls.py
+++ faqs/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('faqs/', views.FAQListCreateView.as_view(), name='faq-list'),
+    path('', views.FAQListCreateView.as_view(), name='faq-list'),
 ]
